ff0f553f45259d88db03b69b2ebe5dd590037279
Doug Major <demajor@demajorMBP.local>
2017-06-22 17:01:44 -0700

    initial commit

---
+++ src/questions.js
@@ -0,0 +1,326 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_GB" : [
+        {
+            "Who composed the original score for the 1975 film Jaws?": [
+                "John Williams",
+                "Trevor Jones",
+                "Jonny Greenwood",
+                "Danny Elfman"
+            ]
+        },
+        {
+            "This composer is a regular collaborator with Tim Burton in his films.": [
+                "Danny Elfman",
+                "Brian Eno",
+                "Ilan Eshkeri",
+                "Hans Zimmer"
+            ]
+        },
+        {
+            "Which composer is best known for his work on the films 'Psycho' & 'Vertigo'?": [
+                "Bernard Herrmann",
+                "Alun Hoddinott",
+                "James Newton Howard",
+                "Gottfried Huppertz"
+            ]
+        },
+        {
+            "Antonio Pinto received high regards for his score work in which documentary about the tragic death of a infamouse Formula One racecar driver?": [
+                "Senna",
+                "Adrift",
+                "Wheels",
+                "Watership Down"
+            ]
+        },
+        {
+            "Filmmaker Wes Andersons' composer favorite Mark Mothersbaugh, was originally a founding member of what 80's new wave band?": [
+                "Devo",
+                "The Cars",
+                "Oingo Boingo",
+                "The Police"
+            ]
+        },
+        {
+            "Darren Aronofsky's main go-to composer for his films (Pi, Requiem For A Dream), that got his beginnings in the industrial/indie rock band Pop Will Eat Itself is?": [
+                "Clint Mansell",
+                "Angelo Badalamenti",
+                "Roy Ayers",
+                "Michael Andrews"
+            ]
+        },
+        {
+            "Which composer and main instrument was used in opening theme for the original television show Twin Peaks?": [
+                "Angelo Badalamenti - synth bass",
+                "Danny Elfman - electric bass",
+                "Ry Cooder - synthesizer",
+                "Stewart Copeland - synth drums"
+            ]
+        },
+        {
+            "This well-known composer is know for their stellar work in films such as 'The Untouchables', and 'A Fistful of Dollars'?": [
+                "Ennio Morricone",
+                "Hideki Naganuma",
+                "David Newman",
+                "Karen O"
+            ]
+        },
+        {
+            "The drummer and composer Stewart Copeland, from the band The Police, composed the score for which hit film?": [
+                "Wall Street",
+                "Money Pit",
+                "Planes, Trains & Automobiles",
+                "Weird Science"
+            ]
+        },
+        {
+            "Jonny Greenwood, of Radiohead fame, is known as a composer on which film?": [
+                "There Will Be Blood",
+                "Gangs Of New York",
+                "Lincoln",
+                "Last Of The Mohicans"
+            ]
+        },
+        {
+            "This composer is best known for his work on The Dark Knight?": [
+                "James Newton Howard",
+                "Hans Zimmer",
+                "John Williams",
+                "Nicholas Hooper"
+            ]
+        },
+        {
+            "Which musican composed the music for the 1972 blaxplotation crime drama 'Super Fly'?": [
+                "Curtis Mayfield",
+                "Roy Ayers",
+                "George Clinton",
+                "Marvin Gaye"
+            ]
+        },
+        {
+            "This composer, well-known for his work on films like 'Midnight Express' and 'Scarface' also recently produced a record with the dance/house group Daft Punk.": [
+                "Giorgio Moroder",
+                "Andrea Morricone",
+                "John Murphy",
+                "David Newman"
+            ]
+        }
+    ],
+    "QUESTIONS_EN_US" : [
+        {
+            "Who composed the original score for the 1975 film Jaws?": [
+                "John Williams",
+                "Trevor Jones",
+                "Jonny Greenwood",
+                "Danny Elfman"
+            ]
+        },
+        {
+            "This composer is a regular collaborator with Tim Burton in his films.": [
+                "Danny Elfman",
+                "Brian Eno",
+                "Ilan Eshkeri",
+                "Hans Zimmer"
+            ]
+        },
+        {
+            "Which composer is best known for his work on the films 'Psycho' & 'Vertigo'?": [
+                "Bernard Herrmann",
+                "Alun Hoddinott",
+                "James Newton Howard",
+                "Gottfried Huppertz"
+            ]
+        },
+        {
+            "Antonio Pinto received high regards for his score work in which documentary about the tragic death of a infamouse Formula One racecar driver?": [
+                "Senna",
+                "Adrift",
+                "Wheels",
+                "Watership Down"
+            ]
+        },
+        {
+            "Filmmaker Wes Andersons' composer favorite Mark Mothersbaugh, was originally a founding member of what 80's new wave band?": [
+                "Devo",
+                "The Cars",
+                "Oingo Boingo",
+                "The Police"
+            ]
+        },
+        {
+            "Darren Aronofsky's main go-to composer for his films (Pi, Requiem For A Dream), that got his beginnings in the industrial/indie rock band Pop Will Eat Itself is?": [
+                "Clint Mansell",
+                "Angelo Badalamenti",
+                "Roy Ayers",
+                "Michael Andrews"
+            ]
+        },
+        {
+            "Which composer and main instrument was used in opening theme for the original television show Twin Peaks?": [
+                "Angelo Badalamenti - synth bass",
+                "Danny Elfman - electric bass",
+                "Ry Cooder - synthesizer",
+                "Stewart Copeland - synth drums"
+            ]
+        },
+        {
+            "This well-known composer is know for their stellar work in films such as 'The Untouchables', and 'A Fistful of Dollars'?": [
+                "Ennio Morricone",
+                "Hideki Naganuma",
+                "David Newman",
+                "Karen O"
+            ]
+        },
+        {
+            "The drummer and composer Stewart Copeland, from the band The Police, composed the score for which hit film?": [
+                "Wall Street",
+                "Money Pit",
+                "Planes, Trains & Automobiles",
+                "Weird Science"
+            ]
+        },
+        {
+            "Jonny Greenwood, of Radiohead fame, is known as a composer on which film?": [
+                "There Will Be Blood",
+                "Gangs Of New York",
+                "Lincoln",
+                "Last Of The Mohicans"
+            ]
+        },
+        {
+            "This composer is best known for his work on The Dark Knight?": [
+                "James Newton Howard",
+                "Hans Zimmer",
+                "John Williams",
+                "Nicholas Hooper"
+            ]
+        },
+        {
+            "Which musican composed the music for the 1972 blaxplotation crime drama 'Super Fly'?": [
+                "Curtis Mayfield",
+                "Roy Ayers",
+                "George Clinton",
+                "Marvin Gaye"
+            ]
+        },
+        {
+            "This composer, well-known for his work on films like 'Midnight Express' and 'Scarface' also recently produced a record with the dance/house group Daft Punk.": [
+                "Giorgio Moroder",
+                "Andrea Morricone",
+                "John Murphy",
+                "David Newman"
+            ]
+        }
+    ],
+    "QUESTIONS_DE_DE" : [
+        {
+            "Who composed the original score for the 1975 film Jaws?": [
+                "John Williams",
+                "Trevor Jones",
+                "Jonny Greenwood",
+                "Danny Elfman"
+            ]
+        },
+        {
+            "This composer is a regular collaborator with Tim Burton in his films.": [
+                "Danny Elfman",
+                "Brian Eno",
+                "Ilan Eshkeri",
+                "Hans Zimmer"
+            ]
+        },
+        {
+            "Which composer is best known for his work on the films 'Psycho' & 'Vertigo'?": [
+                "Bernard Herrmann",
+                "Alun Hoddinott",
+                "James Newton Howard",
+                "Gottfried Huppertz"
+            ]
+        },
+        {
+            "Antonio Pinto received high regards for his score work in which documentary about the tragic death of a infamouse Formula One racecar driver?": [
+                "Senna",
+                "Adrift",
+                "Wheels",
+                "Watership Down"
+            ]
+        },
+        {
+            "Filmmaker Wes Andersons' composer favorite Mark Mothersbaugh, was originally a founding member of what 80's new wave band?": [
+                "Devo",
+                "The Cars",
+                "Oingo Boingo",
+                "The Police"
+            ]
+        },
+        {
+            "Darren Aronofsky's main go-to composer for his films (Pi, Requiem For A Dream), that got his beginnings in the industrial/indie rock band Pop Will Eat Itself is?": [
+                "Clint Mansell",
+                "Angelo Badalamenti",
+                "Roy Ayers",
+                "Michael Andrews"
+            ]
+        },
+        {
+            "Which composer and main instrument was used in opening theme for the original television show Twin Peaks?": [
+                "Angelo Badalamenti - synth bass",
+                "Danny Elfman - electric bass",
+                "Ry Cooder - synthesizer",
+                "Stewart Copeland - synth drums"
+            ]
+        },
+        {
+            "This well-known composer is know for their stellar work in films such as 'The Untouchables', and 'A Fistful of Dollars'?": [
+                "Ennio Morricone",
+                "Hideki Naganuma",
+                "David Newman",
+                "Karen O"
+            ]
+        },
+        {
+            "The drummer and composer Stewart Copeland, from the band The Police, composed the score for which hit film?": [
+                "Wall Street",
+                "Money Pit",
+                "Planes, Trains & Automobiles",
+                "Weird Science"
+            ]
+        },
+        {
+            "Jonny Greenwood, of Radiohead fame, is known as a composer on which film?": [
+                "There Will Be Blood",
+                "Gangs Of New York",
+                "Lincoln",
+                "Last Of The Mohicans"
+            ]
+        },
+        {
+            "This composer is best known for his work on The Dark Knight?": [
+                "James Newton Howard",
+                "Hans Zimmer",
+                "John Williams",
+                "Nicholas Hooper"
+            ]
+        },
+        {
+            "Which musican composed the music for the 1972 blaxplotation crime drama 'Super Fly'?": [
+                "Curtis Mayfield",
+                "Roy Ayers",
+                "George Clinton",
+                "Marvin Gaye"
+            ]
+        },
+        {
+            "This composer, well-known for his work on films like 'Midnight Express' and 'Scarface' also recently produced a record with the dance/house group Daft Punk.": [
+                "Giorgio Moroder",
+                "Andrea Morricone",
+                "John Murphy",
+                "David Newman"
+            ]
+        }
+    ]
+};
